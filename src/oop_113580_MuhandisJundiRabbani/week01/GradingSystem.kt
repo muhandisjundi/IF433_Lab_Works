e6f66ca9bb10e151464c024bbef02cf9408acbbd
muhandisjundi <muhandisjundi05@gmail.com>
2026-02-03 15:43:50 +0700

    week01: initial setup and procedural code

---
+++ src/oop_113580_MuhandisJundiRabbani/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_113580_MuhandisJundiRabbani.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    //Concatenation.gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
