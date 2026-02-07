3a56a54f94240bddff3ddf10e175e0f7d5ecdbe6
muhandisjundi <muhandisjundi05@gmail.com>
2026-02-07 14:52:28 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_113580_MuhandisJundiRabbani/week01/GameStore.kt
+++ src/oop_113580_MuhandisJundiRabbani/week01/GameStore.kt
@@ -3,6 +3,10 @@ package oop_113580_MuhandisJundiRabbani.week01
 fun main() {
     val gameTitle = "Pou"
     val price = 200000
+
+    printReceipt(title = gameTitle, finalPrice = price)
 }
 
 fun calculateDiscount(price: Int): Int = if (price > 500000) (price * 20) / 100  else (price * 10) / 100
+
+fun printReceipt(title: String, finalPrice: Int) {}
