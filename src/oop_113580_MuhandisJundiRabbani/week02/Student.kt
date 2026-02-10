9c85f49bf626102a8380fde5e4fd8dd5e7ae460f
muhandisjundi <muhandisjundi05@gmail.com>
2026-02-10 15:27:51 +0700

    week02: create basic Student class structure

---
+++ src/oop_113580_MuhandisJundiRabbani/week02/Student.kt
@@ -0,0 +1,11 @@
+package oop_113580_MuhandisJundiRabbani.week02
+
+package oop_113580_MuhandisJundiRabbani.week02
+
+class Student(
+    val name: String,
+    val nim: String,
+    var major: String
+){
+    //body class kosong dulu
+}
