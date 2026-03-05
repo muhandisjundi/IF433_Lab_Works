0b8c1ff82cd4763ad3ed00a65010e766326a84d9
muhandisjundi <muhandisjundi05@gmail.com>
2026-03-05 23:23:09 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_113580_MuhandisJundiRabbani/week05/EWallet.kt
@@ -0,0 +1,16 @@
+package oop_113580_MuhandisJundiRabbani.week05
+
+class EWallet(var balance: Double) : PaymentMethod("EWallet") {
+    override fun processPayment(amount: Double) {
+        if (balance >= amount) {
+            balance - amount
+            println("Pembayaran Sukses. Balance : $balance")
+        } else {
+            println("Saldo tidak cukup")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+    }
+}
