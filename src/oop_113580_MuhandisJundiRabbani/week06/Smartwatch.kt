7f3c427a1a01e79bb2f6661f5fe416f442bcd045
muhandisjundi <muhandisjundi05@gmail.com>
2026-03-10 14:59:06 +0700

    week06: create Smartwatch with multiple implementations

---
+++ src/oop_113580_MuhandisJundiRabbani/week06/Smartwatch.kt
@@ -0,0 +1,16 @@
+package oop_113580_MuhandisJundiRabbani.week06
+
+//menggabungkan 1 parent class dan 2 interfaces
+class Smartwatch : Watch(), BluethoothConnectable, Recahrgable {
+    override fun showTime() {
+        println("layar oled menyala: 14:00 WIB")
+    }
+
+    override fun connectToBluethooth() {
+        println("mencari perangkat HP di sekitar untuk pairing..."
+    }
+
+    override fun chargeBattery() {
+        println("mengisi daya menggunakan charger magnetik 15W")
+    }
+}
