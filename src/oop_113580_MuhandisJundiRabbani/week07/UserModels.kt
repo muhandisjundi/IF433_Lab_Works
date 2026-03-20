a3c553939bb3b9e67e7b626994e9c8a7c299003b
muhandisjundi <muhandisjundi05@gmail.com>
2026-03-20 14:30:58 +0700

    week07: implement data class for structural equality

--- src/oop_113580_MuhandisJundiRabbani/week07/UserModels.kt
+++ src/oop_113580_MuhandisJundiRabbani/week07/UserModels.kt
@@ -1,5 +1,5 @@
 package oop_113580_MuhandisJundiRabbani.week07
 
-class RegularUser(val name: String, val age: Int) {
+class RegularUser(val name: String, val age: Int)
 
-}
+data class DataUser(val name: String, val age: Int)
