3420196260bc1430f47bf6f9a30dea0f3f918683
muhandisjundi <muhandisjundi05@gmail.com>
2026-04-15 22:06:18 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_113580_MuhandisJundiRabbani/week08/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_113580_MuhandisJundiRabbani.week08;
+
+public class LegacyJavaAPI {
+    //fungsi ini bentuk java. kotlin tidak tau apakah return nya nullable
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_2--_OK";
+    }
+}
