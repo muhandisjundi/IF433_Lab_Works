91bb375f8e0d31a1288170f943a301d704ea65e0
muhandisjundi <muhandisjundi05@gmail.com>
2026-04-20 21:42:20 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_113580_MuhandisJundiRabbani/week08/EcommerceModels.kt
@@ -0,0 +1,6 @@
+package oop_113580_MuhandisJundiRabbani.week08
+
+sealed class Product{
+    data class Electronic(val id: String, val name: String, val warrantyMonths: Int) : Product()
+    data class Clothing(val id: String, val name: String, val size: String) : Product()
+}
