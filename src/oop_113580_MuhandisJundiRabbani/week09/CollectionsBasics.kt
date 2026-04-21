9e2251ef8041915afa71cb2aa1a00caea2a886f4
muhandisjundi <muhandisjundi05@gmail.com>
2026-04-21 15:23:37 +0700

    week09: implement set to discard duplicate elements

--- src/oop_113580_MuhandisJundiRabbani/week09/CollectionsBasics.kt
+++ src/oop_113580_MuhandisJundiRabbani/week09/CollectionsBasics.kt
@@ -1,5 +1,7 @@
 package oop_113580_MuhandisJundiRabbani.week09
 
+import oop_113580_MuhandisJundiRabbani.week01.printReceipt
+
 fun main(){
     println("=== TEST LIST ===")
     //immuttable list: tidak bisa diubah setelah dibuat
@@ -12,4 +14,14 @@ fun main(){
     scores.add(95)
     scores[0] = 88
     println("mutable list: $scores")
+
+
+    println("\n=== TEST SET ===")
+    val uniqueNumbers = setOf(1,2,2,3,3,4)
+    println("uniqueNumbers (set): $uniqueNumbers") //duplikat hilang
+
+    val activeUsers = mutableSetOf("userA", "userB")
+    activeUsers.add("userC")
+    activeUsers.add("userA") //diabaikan karena sudah ada
+    println("active users: $activeUsers")
 }
