e2c456040b9969a572b1b3ed2ed5a8a361c88a6c
muhandisjundi <muhandisjundi05@gmail.com>
2026-04-21 15:45:17 +0700

    week09: implement sortedByDescending and forEach

--- src/oop_113580_MuhandisJundiRabbani/week09/HigherOrderFunctions.kt
+++ src/oop_113580_MuhandisJundiRabbani/week09/HigherOrderFunctions.kt
@@ -7,4 +7,12 @@ fun main(){
     //filter() membuar list baru berisi elemen yang memenuhi kondisi true
     val evens = numbers.filter { it % 2 == 0 }
     println("evens only: $evens")
+
+
+    println("\n=== HOF: MAP ===")
+    //map() merubah bentuk/nilai data, tapi ukurannca tetap sama
+    val multiplied = evens.map { it * 10 }
+    val asStrings = multiplied.map { "Rp ${it}" }
+    println("multiplied: $multiplied")
+    println("formatted: $asStrings")
 }
