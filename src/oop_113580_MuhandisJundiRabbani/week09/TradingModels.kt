79e33ac4c94d94a41a25152cfacd169fc7a58d5c
muhandisjundi <muhandisjundi05@gmail.com>
2026-04-22 22:38:21 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_113580_MuhandisJundiRabbani/week09/TradingModels.kt
@@ -0,0 +1,5 @@
+package oop_113580_MuhandisJundiRabbani.week09
+
+import javax.swing.plaf.nimbus.State
+
+data class TradeLog(val pair: String, val position: String, val leverage: Int, val roe: Double, val status: String)
