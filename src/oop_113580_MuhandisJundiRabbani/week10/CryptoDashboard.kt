3d76c6156655d419d965d3e5d615a102f070d99c
muhandisjundi <muhandisjundi05@gmail.com>
2026-04-28 15:59:03 +0700

    week10: (task) populate coin repository

--- src/oop_113580_MuhandisJundiRabbani/week10/CryptoDashboard.kt
+++ src/oop_113580_MuhandisJundiRabbani/week10/CryptoDashboard.kt
@@ -4,5 +4,8 @@ package oop_113580_MuhandisJundiRabbani.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 2.0))
+    coinRepo.add(Coin("USDT", 1000.0))
 
 }
