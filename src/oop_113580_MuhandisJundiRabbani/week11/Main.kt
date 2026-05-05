c3597ab1a66b8f7211e13605634fa665236742e8
muhandisjundi <muhandisjundi05@gmail.com>
2026-05-05 15:32:18 +0700

    week11: combine apply and also in object setup

--- src/oop_113580_MuhandisJundiRabbani/week11/Main.kt
+++ src/oop_113580_MuhandisJundiRabbani/week11/Main.kt
@@ -32,8 +32,16 @@ fun main() {
     }.add(4)
     println("Setelah ditambah: $numbers")
 
+
     println("\n=== TEST WITH ===")
     with(user) {
         println("User Detail -> Nama: $name, Umur: $age")
     }
+
+    val newUser = User("Budi", 20).apply {
+        age = 21
+    }.also {
+        println("User baru berhasil dibuat: $it")
+    }
+}
 }
