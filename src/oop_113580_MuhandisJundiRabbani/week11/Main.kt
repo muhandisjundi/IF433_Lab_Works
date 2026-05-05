dd296985266be8a859f3c8d59e4e7eabb0120920
muhandisjundi <muhandisjundi05@gmail.com>
2026-05-05 15:30:03 +0700

    week11: implement apply for object initialization

--- src/oop_113580_MuhandisJundiRabbani/week11/Main.kt
+++ src/oop_113580_MuhandisJundiRabbani/week11/Main.kt
@@ -15,4 +15,13 @@ fun main() {
         length * 2
     }
     println("Hasil kalkulasi run: $result")
+
+
+
+    println("=== TEST APPLY ===")
+    val user = User().apply {
+        name = "Alex"
+        age = 25
+    }
+    println(user)
 }
