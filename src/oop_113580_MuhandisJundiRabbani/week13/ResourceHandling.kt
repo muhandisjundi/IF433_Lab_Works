3c21bbf1550fab3c5f88a8464a4c78e809d3802e
muhandisjundi <muhandisjundi05@gmail.com>
2026-05-26 14:53:28 +0700

    week13: implement manual close for file stream

---
+++ src/oop_113580_MuhandisJundiRabbani/week13/ResourceHandling.kt
@@ -0,0 +1,17 @@
+package oop_113580_MuhandisJundiRabbani.week13
+
+import java.io.File
+
+fun main() {
+    println("=== TEST UNSAFE RESOURCE HANDLING ===")
+    val unsafeFile = File("unsafe_logs.txt")
+
+    // Membuka stream secara manual
+    val writer = unsafeFile.printWriter()
+    writer.println("Log 1: Membuka koneksi database...")
+    writer.println("Log 2: Menulis data pengguna...")
+
+    // Wajib dipanggil secara manual jika tidak memakai blok 'use'
+    writer.close()
+    println("Proses penulisan unsafe selesai.")
+}
